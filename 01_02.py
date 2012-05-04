c88436678aec8f333e0320ba9df999d5e0984771
slaer <slaer93@gmail.com>
2012-05-04 22:56:24 +0400

    update

--- 01_02.py
+++ 01_02.py
@@ -1,17 +1,26 @@
 __author__ = 'slaer'
 L = (4, 23, 23, 12, 16, 56, 18, 13, 23, 24, 17, 34, 12, 34, 5, 6, 23, 23, 81, 14)
+count,maxCount,maxNumber,i,k,x,number = 0,0,0,0,0,0,[]
+
 L1 = list(L)
 L1.sort()
-x = 0
-k = 0
-print L1
 
+print L1
 for i in L1:
     if i == x:
-        print i
-        k += 1
-
-
+        number.append(i)
     x = i
-print k
+
+for i in L1:
+    if i in number:
+        count += 1
+        if count < maxCount:
+            pass
+        else:
+            maxCount = count
+            maxNumber = i
+    else:
+        count = 0
+
+print {'Number': maxNumber, 'Counts': maxCount}
 
